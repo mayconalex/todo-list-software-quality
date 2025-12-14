213e46bbaec2f9489481f35726e05bef0f247108
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2025-12-14 15:28:47 -0300

    :bug: fix: desabilita rule para o próprio arquivo de configuração

--- eslint.config.ts
+++ eslint.config.ts
@@ -22,9 +22,12 @@ export default defineConfigWithVueTs(
 
   {
     rules: {
-      'complexity': ['error', 5], // Smell: Complexidade Ciclomática
-      'max-params': ['error', 3], // Smell: Muitos Parâmetros
-      'no-magic-numbers': ['error', { ignore: [0, 1, -1], ignoreArrayIndexes: true }] // Smell: Números Mágicos
+      // Smell: Complexidade Ciclomática
+      'complexity': ['error', 5], // eslint-disable-line no-magic-numbers
+      // Smell: Muitos Parâmetros
+      'max-params': ['error', 3], // eslint-disable-line no-magic-numbers
+      // Smell: Números Mágicos
+      'no-magic-numbers': ['error', { ignore: [0, 1, -1], ignoreArrayIndexes: true }] 
     }
   }
 )
