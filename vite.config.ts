eb0a39f06fde8e0777cd2313846cbb65cff00597
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2026-01-06 14:32:21 -0300

    :sparkles: feat: habilita https usando self-signed certificate

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'node:fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
+    server: {
+        https: {
+            key: fs.readFileSync('./private.key'),
+            cert: fs.readFileSync('./certificate.pem'),
+        }
+    }
 })
